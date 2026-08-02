90607fca0e37fd7cd684a7b34d6063f1684aa31d
hashimWIFI <hashim.mohamed.tz@gmail.com>
2026-08-02 17:57:24 +0300

    Update notification text for Wi-Fi package purchase

--- login.php
+++ login.php
@@ -304,7 +304,7 @@ function startPaymentVerificationLoop() {
                     // Dynamically map amount and period confirmation notification text strings
                     var subtextElement = document.getElementById('payment-subtext');
                     if (subtextElement) {
-                        subtextElement.innerHTML = "Hongera! Umefanikiwa kununua kifurushi cha Wi-Fi cha <b>Tsh " + parseInt(planAmount).toLocaleString() + "</b> kitakachotumika kwa muda wa <b> " + planDuration + ".</b>"; 
+                        subtextElement.innerHTML = "Hongera! Umefanikiwa kununua kifurushi cha Wi-Fi cha <b><br>Tsh " + parseInt(planAmount).toLocaleString() + "</b> kitakachotumika kwa muda wa <b> " + planDuration + ".</b>"; 
                     }
                     
                     // INLINE REVEAL LOGIC: Capture our twin structural layer elements safely
