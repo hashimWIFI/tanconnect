959aa4d4f858517b5225bd603979dc6c0678eab5
hashimWIFI <hashim.mohamed.tz@gmail.com>
2026-08-06 07:57:40 +0300

    Update sms_processor.php

--- sms_processor.php
+++ sms_processor.php
@@ -21,7 +21,7 @@ if (!empty($customer_phone)) {
     echo "Target Customer Recipient: " . $customer_phone . "\n";
     
     // Read cleanly from Railway environment variables
-    $textbee_api_key = getenv('TEXTBEE_API_KEY') ?: 'txb_nr4AZvvZoncnKwhsgTKJufStKToas52g'; 
+    $textbee_api_key = getenv('TEXTBEE_API_KEY') ?: 'txb_o8CjmlHiTYv7KVSGwWYi8sE3W9W9OhJS'; 
     $textbee_device_id = getenv('TEXTBEE_DEVICE_ID') ?: '6a70f731f83fbea6290c1fff'; 
     
     // Fallbacks to default values if package metadata isn't logged in row
