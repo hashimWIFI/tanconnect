8041a6133a1c93c492ef6559997ed0361c4f5148
hashimWIFI <hashim.mohamed.tz@gmail.com>
2026-08-08 19:31:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -360,7 +360,7 @@ function dispatchToRailway(event) {
             <div style="margin: 0; color: #1e293b; font-size: 14px; font-weight: bold;"><img src="water.png" alt="Water Point Logo" style="max-width: 150px; height: auto; object-fit: contain;">
 
 
-            <p style="margin: 2px 0 0 0; color: #64748b; font-size: 10px; text-align: center;">Karibu ujipatie maji safi yaliyochujwa kwa technolojia ya kisasa ya RO na UV.<br><br><b>Tunapatikana TANDIKA, Mtaa MALUMBA</p></b></div>
+            <p style="margin: 2px 0 0 0; color: #64748b; font-size: 10px; text-align: center;">Karibu ujipatie maji safi yaliyochujwa kwa technolojia ya kisasa ya RO na UV.<br><br><b>Tunapatikana TANDIKA, Mtaa MALUMBA</p></b></div></div></div>
 <hr width="100%" align="center"> </div></div></body><div>
  
  <div class="footer">Unaweza kuwasiliana nasi kwa nambari 0713 123 974 au kwa kutembelea tovuti yetu "www.tanconnect.co.tz".</div> </div>
