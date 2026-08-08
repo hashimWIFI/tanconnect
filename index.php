953a34499d21159a7f981027acf9b6a4c2da867b
hashimWIFI <hashim.mohamed.tz@gmail.com>
2026-08-08 16:48:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -352,8 +352,7 @@ function dispatchToRailway(event) {
 }
 
 </script>
-
-
+<br><br><br>
 
 <div style="margin: 8px auto; max-width: 450px; background: #ffffff; border-left: 10px solid #3498db; border-top: 1px solid #e2e8f0; border-right: 1px solid #e2e8f0; border-bottom: 1px solid #e2e8f0; border-radius: 0 10px 10px 0; padding: 10px 10px; display: flex; align-items: center; justify-content: space-between; gap: 10px; font-family: sans-serif;">
     <div style="display: flex; align-items: center; gap: 12px;">
