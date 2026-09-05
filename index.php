39668fa4f6aa833023044b5f46d8d80df6f36fbd
hashimWIFI <hashim.mohamed.tz@gmail.com>
2026-09-05 15:20:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -376,8 +376,10 @@ ini_set('display_errors', 0);
     </p>
 
     <p style="margin: 0; font-size: 9px; color: #777777; line-height: 1.6;">
-           <strong>Customer Support Desk:</strong><br>
-        📧<b> Email: <a href="mailto:support@tanconnect.co.tz" style="color: #0066cc; text-decoration: none;">support@tanconnect.co.tz</a> &nbsp;&nbsp;|&nbsp;&nbsp; 📞 Phone: +255 713 123 974
+        <strong>Customer Support Desk:</strong><br>
+        📧<b> Email: <a href="mailto:support@tanconnect.co.tz" style="color: #0066cc; text-decoration: none;">support@tanconnect.co.tz</a> &nbsp;&nbsp;|&nbsp;&nbsp; 📞 Phone: <a href="tel:+255713123974" style="color: #0066cc; text-decoration: none;">+255 713 123 974</a>
+
+
     </p></b>
 </footer>
 <!-- END OF AZAMPAY MANDATORY COMPLIANCE FOOTER -->
