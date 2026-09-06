32bfbdbee9264594abb4ca51a21c2cd36c1a2d85
hashimWIFI <hashim.mohamed.tz@gmail.com>
2026-09-06 18:35:33 +0300

    Update terms.php

--- terms.php
+++ terms.php
@@ -25,7 +25,7 @@ By executing a commercial purchase request on TANConnect<sup style="font-family:
 
 <a href="/" class="btn-home" style="background: darkblue; width: 100%; box-sizing: border-box; text-decoration: none;">← RUDI NYUMA (BACK HOME)</a>
 <footer style="margin-top: 5px; padding: 10px 10px; text-align: center; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif; border-radius: 8px; font-size: 10px; color: #555555; background-color: #fafafa;">
- <p style="margin: 0 0 10px 0; " <b> © 2026 NIT Africa Solutions Ltd.</b> All Rights Reserved. <br>TANConnect<sup style="font-family: Arial, Helvetica, sans-serif; font-size: 6px; font-weight: normal; vertical-align: super; line-height: 0;">&reg;</sup> is a registered trademark of<br><a href= https://nitafricasolutions-production-2f54.up.railway.app style="color: #0066cc; text-decoration: none; font-weight: 500;"> NIT Africa Solutions Ltd</a></p>
+ <p> <b> © 2026 NIT Africa Solutions Ltd.</b> All Rights Reserved. <br>TANConnect<sup style="font-family: Arial, Helvetica, sans-serif; font-size: 6px; font-weight: normal; vertical-align: super; line-height: 0;">&reg;</sup> is a registered trademark of<br><a href= https://nitafricasolutions-production-2f54.up.railway.app style="color: #0066cc; text-decoration: none; font-weight: 500;"> NIT Africa Solutions Ltd</a></p>
 
 </div>
 
