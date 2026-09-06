aed423a3cf16f68454f50ec8337fba4351470784
hashimWIFI <hashim.mohamed.tz@gmail.com>
2026-09-06 18:40:14 +0300

    Refactor footer styles and layout adjustments
    Updated footer styles and adjusted padding for better layout.

--- refund.php
+++ refund.php
@@ -24,8 +24,8 @@
  All micro-payments done to purchase local Wi-Fi digital voucher tokens are completed following receipt of a valid "SUCCESS" webhook payload validation check from AzamPay servers. If an operational infrastructure breakdown happens where customer money is withdrawn but an SMS with voucher token is delayed, you can contact support team to intervene. Support team will cross-reference your validation identifier and issue your voucher token manually.</p>
 
 <a href="/" class="btn-home" style="background: darkblue; width: 100%; box-sizing: border-box; text-decoration: none;">← RUDI NYUMA (BACK HOME)</a>
-<footer style="margin-top: 15px; padding: 15px 15px; text-align: center; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif; border-radius: 8px; font-size: 9px; color: #555555; background-color: #fafafa;">
-  <p style="margin: 0 0 10px 0; font-weight: bold;"> © 2026 NIT Africa Solutions Limited. All Rights Reserved. <br>TANConnect<sup style="font-family: Arial, Helvetica, sans-serif; font-size: 6px; font-weight: normal; vertical-align: super; line-height: 0;">&reg;</sup> is a registered trademark of<a href= https://nitafricasolutions-production-2f54.up.railway.app style="color: #0066cc; text-decoration: none; font-weight: 500;"> NIT Africa Solutions Limited</a></p>
+<footer style="margin-top: 5px; padding: 10px 10px; text-align: center; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif; border-radius: 8px; font-size: 10px; color: #555555; background-color: #fafafa;">
+ <p> <b> © 2026 NIT Africa Solutions Ltd.</b> All Rights Reserved. <br>TANConnect<sup style="font-family: Arial, Helvetica, sans-serif; font-size: 6px; font-weight: normal; vertical-align: super; line-height: 0;">&reg;</sup> is a registered trademark of<br><a href= https://nitafricasolutions-production-2f54.up.railway.app style="color: #0066cc; text-decoration: none; font-weight: 500;"> NIT Africa Solutions Ltd</a></p>
 
 </div>
  
