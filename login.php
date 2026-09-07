9db481dc792aa67ed83551b77417e2eedbc9de7b
hashimWIFI <hashim.mohamed.tz@gmail.com>
2026-09-07 22:30:21 +0300

    Update login.php

--- login.php
+++ login.php
@@ -93,7 +93,7 @@ if ( ! $dbResult ) {
         // 3. SERVER CONFIGURATION FOR ZOHO MAIL
         $mail->isSMTP();
         
-        $mail->Host = smtp.zoho.com;
+        $mail->Host = '://zoho.com';
         $mail->SMTPAuth   = true;
         $mail->Username   = 'support@tanconnect.co.tz';
         $mail->Password   = getenv( 'ZOHO_MAIL_PASSWORD' );
