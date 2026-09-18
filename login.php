d36529cd6ab9206bc603abd3921ccb8f7f9f9775
hashimWIFI <hashim.mohamed.tz@gmail.com>
2026-09-18 14:14:13 +0300

    Update login.php

--- login.php
+++ login.php
@@ -391,11 +391,10 @@ function copyVoucherToClipboard() {
     var pinText = document.getElementById("raw-pin-string").innerText;
     navigator.clipboard.writeText(pinText).then(function() {
         alert("Voucher yako imenakiliwa! Bonyeza HODI kwenye ukurasa unaofuata, kisha ingiza/ PASTE namba ya voucher yako kuingia mtandaoni.");
-        window.location.href = "http://na.solnms.net/SOL/rechargeMobileManage.do?device_id=' + device_id + '&mac_address=' + mac + '&language=en&billType=0&roamingFlag=0&billing_mode=0";
-					
+        window.location.href = "https://www.5wifi.net";
 
     }, function() {
-        window.location.href = "http://na.solnms.net/SOL/rechargeMobileManage.do?device_id=' + device_id + '&mac_address=' + mac + '&language=en&billType=0&roamingFlag=0&billing_mode=0";
+        window.location.href = "https://www.5wifi.net";
     });
 }
 
