b8653e8a64460b4daf48c2aad3e84d29ae639288
hashimWIFI <hashim.mohamed.tz@gmail.com>
2026-09-18 12:38:14 +0300

    Add files via upload

---
+++ btn_topup.php
@@ -0,0 +1,40 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<title>SOL Hotspot &gt; Login</title>
+<meta http-equiv="Content-Language" content="en-us">
+<meta http-equiv="pragma" content="no-cache">
+<meta http-equiv="expires" content="-1">
+<link rel="stylesheet" type="text/css" href="./SOL Hotspot _ Login_files/screen.css" media="screen">
+<script type="text/javascript" src="./SOL Hotspot _ Login_files/login.js.download"></script> <!-- DO NOT CHANGE ANY TEXT IN THIS LINE  -->
+
+<script src="html5.js"></script>
+<script src="css3-mediaqueries.js"></script>
+</head>
+
+<body>
+<div id="wrapper">
+	<div class="hidden">
+        
+  </div>
+<div id="content" class="clearfix">
+
+  <section id="main_content" class="clearfix">
+ <article id="content4">
+                <form name="login" action="https://www.5wifi.net/login" method="post">
+                    <input name="dst" type="hidden" value="">
+                    <input name="popup" type="hidden" value="true">
+                    <!--<p id="error"></p> -->
+                    <input name="username" type="hidden" id="username">
+                    <input name="password" type="hidden" value="">
+                    <input type="hidden" name="submit" value="Log in" id="submit">
+                </form>
+                <p style="text-align:left; margin: 10px auto;"><a href="javascript:void(0);" onclick="return recharge(&#39;8600081897&#39;,&#39;24:EE:9A:7A:91:12&#39;);" id="topup_button">TOP-UP</a></p>
+            </article>
+
+        </section>
+</body>
+</html>
+
