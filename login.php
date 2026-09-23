1cccdc42638f771023c91e4de9b4e59136bbdf5b
hashimWIFI <hashim.mohamed.tz@gmail.com>
2026-09-23 13:29:46 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,9 @@
 <?php
+// Place this at the very top of login.php to prevent undefined variable crashes
+if (!isset($httpStatusCode)) {
+    $httpStatusCode = 0; 
+}
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
