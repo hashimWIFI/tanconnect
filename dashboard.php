3c7dc457df7fe2784ef8723c7aca3c9a19371119
hashimWIFI <hashim.mohamed.tz@gmail.com>
2026-09-24 10:58:16 +0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+ date_default_timezone_set('Africa/Dar_es_Salaam');
+
 
 // Basic HTTP Authentication Layer for Administrator Access Protection
 $admin_user = "admin";
