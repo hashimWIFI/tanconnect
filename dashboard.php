ec142fd4ba0556bec236d1aa76085dec61cd0bfb
hashimWIFI <hashim.mohamed.tz@gmail.com>
2026-09-26 01:54:29 +0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// 🔐 Start session tracking safely at the absolute beginning
+// 🔐 Start session tracking safely at the absolute beginning 
 session_start();
 
 // 🔑 Define your two entry gate passwords here
